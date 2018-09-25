4eeec078fe031c8fe6bfbe9395de47b6c1a721d2
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-09-25 13:53:27 -0500

    Added jump function in script.

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -5,33 +5,43 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour {
 
     public Rigidbody2D MyRigidBody;
- 
 
-	// Use this for initialization
+    [SerializeField]
+    private int speed;
+   
+
+    // Use this for initialization
 	void Start ()
     {
         Debug.Log("This is start.");
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //Debug.Log("What's up fuckers?");
 
         if (Input.GetKey(KeyCode.D))
         {
             //Move Character to the right. 
-            MyRigidBody.velocity = new Vector2(5, MyRigidBody.velocity.y);
+            MyRigidBody.velocity = new Vector2(speed, MyRigidBody.velocity.y);
 
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             //Move Character to the left. 
-            MyRigidBody.velocity = new Vector2(-5, MyRigidBody.velocity.y);
+            MyRigidBody.velocity = new Vector2(-speed, MyRigidBody.velocity.y);
 
         }
 
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Move Character upwards. 
+            MyRigidBody.velocity = new Vector3(0,10, MyRigidBody.velocity.x);
+
+        }
+
+
     }
 }
