616a332da2d41a81ad03855c4355fb7f10f6fa2d
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-10-16 15:13:39 -0500

    Add door that leads to level two, and add character as prefab

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,14 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    private void OnTriggerStay2D(Collider2D collision)
+    private bool isPlayerInTrigger = false;
+
+    //Can't use this, triggers door twice.
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if(collision.CompareTag("DoorLook") && Input.GetButtonDown("Activate"))
+    //    {
+    //
+    //        Debug.Log("Player activated door.");
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player") && Input.GetKey(KeyCode.W))
+        if(collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
         {
             Debug.Log("Player activated door.");
+
+            SceneManager.LoadScene("Level 2");
+
         }
     }
 
