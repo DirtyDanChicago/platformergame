630bd8ad0a180d82d5b6b08d9983df78b13fad39
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-10-16 14:39:11 -0500

    Made door to lead to second level

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Player activated door.");
+        }
+    }
+
+}
