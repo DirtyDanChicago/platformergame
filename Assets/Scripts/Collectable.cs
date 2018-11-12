4d83c7f422c9647fe73ce59f69cd1c6e6cc59f36
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-11-12 16:06:26 -0600

    Disable sprite renderer, and collider on collision

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    private AudioSource pickUp;
+    private SpriteRenderer carrot;
+    private BoxCollider2D carrotCollider;
+
+    private void Start()
+    {
+        pickUp = GetComponent<AudioSource>();
+        carrot = GetComponent<SpriteRenderer>();
+        carrotCollider = GetComponent<BoxCollider2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.gameObject.CompareTag("Player"))
-        {
-            Destroy(gameObject);
+        {          
+            pickUp.Play();
+
+            carrot.enabled = false;
+
+            carrotCollider.enabled = false;
+
+            Destroy(gameObject, pickUp.clip.length);
         }
     }
 }
