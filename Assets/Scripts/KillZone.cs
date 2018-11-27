8a6008e56d4aab61541d36d9db75bfb25faf4d6f
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-11-27 15:03:36 -0600

    Got kill script up.

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillZone : MonoBehaviour {
+public class KillZone : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        MovementScript player = collision.GetComponent<MovementScript>();
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the trigger.");
+
+            player.Injury();
+
+
+        }
+        else
+        {
+            Debug.Log("Something other than the player entered the trigger.");
+        }
+    }
+
+
 }
+
