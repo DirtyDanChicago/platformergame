98fe69586403c3029b01f4b5e5f9966d5b363aec
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-12-10 21:47:09 -0600

    Intro screen done.

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    public void SceneLoad()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    
+}
